03b3ce51b67dbea9ab6c0415c2f65ec2260aace4
DominikSukar <dominik.sukar@gmail.com>
2024-07-16 23:51:38 +0200

    Add endpoint for retrieving summoner info

--- backend/routers/account.py
+++ backend/routers/account.py
@@ -21,6 +21,7 @@ if not API_KEY or not DOMAIN:
 router = APIRouter()
 
 router_api_url = DOMAIN + "/riot/account/v1/accounts/by-riot-id"
+router_api_url_puuid = DOMAIN + "/riot/account/v1/accounts/by-puuid"
 api_key_url = f"?api_key={API_KEY}"
 
 
@@ -31,7 +32,7 @@ def get_summoner_puuid(summoner_name: str, tag_line: str):
 
     response: str = requests.get(summoner_url)
     if not response.status_code == 200:
-        logger.error("Could not get summoner puuid. Error: {}".format(response))
+        logger.error("Could not get summoner puuid. Error: {}".format(response.text))
         return HTTPException(
             status_code=503, detail="Failed to retrieve data from Riot's API"
         )
@@ -43,18 +44,26 @@ def get_summoner_puuid(summoner_name: str, tag_line: str):
     return summoner_data
 
 
-@router.get("/puuid/")
-async def get_summoner(
+@router.get("/info/")
+async def get_summoner_info(
     nickname: str = Query(None), tag: str = Query(None), puuid: str = Query(None)
 ):
     """
     Returns data about a summoner by their nickname+tag or PUUID.
     """
     if nickname and tag:
-        print("if")
-    elif puuid:
-        print("elif")
-    else:
-        print("else")
+        puuid = get_summoner_puuid(nickname, tag)
 
-    return "Yeah bro"
+    account_info_url = f"{router_api_url_puuid}/{puuid}" + api_key_url
+
+    response: str = requests.get(account_info_url)
+    if not response.status_code == 200:
+        logger.error(f"Failed to retrieve data from Riot's API. Error: {response.text}. URL: {account_info_url}")
+        return HTTPException(
+            status_code=503, detail="Failed to retrieve data from Riot's API"
+        )
+
+    raw_data: str = response.text
+    summoner_info = json.loads(raw_data)
+
+    return summoner_info
