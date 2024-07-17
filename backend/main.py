67c03ce03022d78c447547956754965e14d01688
DominikSukar <dominik.sukar@gmail.com>
2024-07-17 23:47:21 +0200

    Remove env imports in main, change logging config level to debug

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,15 @@
-import os
 import logging
 
 from fastapi import FastAPI
-from dotenv import load_dotenv
 
 from routers import account, match, spectator
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.DEBUG,
     format="%(levelname)s:      %(message)s",
     handlers=[logging.StreamHandler()],
 )
 logger = logging.getLogger(__name__)
-
-load_dotenv()
-API_KEY = os.getenv("API_KEY")
-DOMAIN = os.getenv("DOMAIN")
 
 app = FastAPI()
 
