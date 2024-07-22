5a186fe5683c512ffd54dafcbe1b70c03f748f71
DominikSukar <dominik.sukar@gmail.com>
2024-07-22 22:23:52 +0200

    Prepare match history endpoint for frontend tests

--- backend/api_requests/match.py
+++ backend/api_requests/match.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 
 class MatchController:
     "Class manages the Riot's API 'MATCH-V5' service. As of 07.20.2024 there are 3 endpoints."
-    
+
     DOMAIN = DOMAIN_EUROPE + "/lol/match/v5/matches"
     key = f"?api_key={API_KEY}"
 
@@ -19,7 +19,6 @@ class MatchController:
     )
     url_match = "{}/{}{}".format(DOMAIN, "{match_id}", key)
     url_match_timeline = "{}/{}/timeline{}".format(DOMAIN, "{match_id}", key)
-
 
     def get_a_list_of_match_ids_by_puuid(self, puuid: str) -> MatchIds:
         "Endpoint gets you a list of user's matches"
--- backend/routers/match.py
+++ backend/routers/match.py
@@ -16,6 +16,8 @@ async def match_history(nickname: str = None, tag: str = None, puuid: str = None
     """Returns user's match history by provided puuid.
     It is also possible to provide simple nickname and tag,
     however additional request is made by API, making response slower."""
+    NUM_OF_GAMES_TO_FETCH = 20
+
     if not puuid and not (nickname and tag):
         raise HTTPException(
             status_code=400,
@@ -28,8 +30,25 @@ async def match_history(nickname: str = None, tag: str = None, puuid: str = None
 
     controller = MatchController()
     match_ids = controller.get_a_list_of_match_ids_by_puuid(puuid)
+    
+    data_to_return = []
 
     for match_id in match_ids:
-        match = controller.get_a_match_by_match_id(match_id)
+        match_data = controller.get_a_match_by_match_id(match_id)
 
-        return match
+        participants = match_data["info"]["participants"]
+        
+        for participant in participants:
+            if participant["puuid"] == puuid:
+                matched_participant = {
+                    "win": participant["win"],
+                    "championId": participant["championId"],
+                    "championName": participant["championName"],
+                    "individualPosition": participant["individualPosition"],
+                    "teamId": participant["teamId"],
+                }
+                break
+
+        data_to_return.append(matched_participant)
+
+    return data_to_return
