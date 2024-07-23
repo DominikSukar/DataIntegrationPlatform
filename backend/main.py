c9bb7e787ba029777d4f9861c275117ac121916c
DominikSukar <dominik.sukar@gmail.com>
2024-07-23 20:53:44 +0200

    Add CORS handling to backend

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ import logging
 import requests
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routers import account, match, spectator, summoner
 
@@ -15,6 +16,19 @@ logging.getLogger("requests").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(account.router, tags=["Account"], prefix="/account")
 app.include_router(summoner.router, tags=["Summoner"], prefix="/summoner")
