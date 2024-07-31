ad66f167f000565dd04af813e3f923e471eb27ea
DominikSukar <dominik.sukar@gmail.com>
2024-07-31 23:28:55 +0200

    Add match games to user, make component client side

--- frontend/app/(root)/user/page.tsx
+++ frontend/app/(root)/user/page.tsx
@@ -1,9 +1,46 @@
+"use client"
+import { useEffect, useState } from 'react';
+
+import Game  from "@/components/Game"
+
+interface matchData {
+  win: boolean;
+  championId: number;
+  championName: string;
+  individualPosition: string;
+  teamId: number;
+}
 
 const User = ({searchParams}: {searchParams: {summonername: string, tag: string}}) => {
   const summonerName = searchParams.summonername
   const tag = searchParams.tag
+
+  const [matches, setMatches] = useState<matchData[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`http://localhost:8000/match_history?nickname=${summonerName}&tag=${tag}`);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data: matchData[] = await response.json();
+        setMatches(data);
+      } catch (error) {
+        console.error('Fetch error:', error);
+      }
+    };
+
+    fetchData();
+  }, [summonerName, tag]);
+
   return (
-    <div>{summonerName} # {tag}</div>
+    <div>
+      <div>{summonerName} # {tag}</div>
+      {matches.map((match, index) => (
+        <Game key={index} win={match.win} championId={match.championId} championName={match.championName} individualPosition={match.individualPosition} />
+      ))}
+    </div>
   )
 }
 
