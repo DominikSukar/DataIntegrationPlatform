84faeb7c13f23dd7e94f3e6f278b98f4989da4fb
DominikSukar <dominik.sukar@gmail.com>
2024-08-11 21:23:12 +0200

    feat: make itemicon component return blank places when ID equals 0

--- frontend/components/game/ItemIcon.tsx
+++ frontend/components/game/ItemIcon.tsx
@@ -1,6 +1,11 @@
 import Image from 'next/image';
 
 function ItemIcon({ itemID, size }: {itemID: number, size: number}) {
+  if (itemID === 0) {
+    return (
+      <div className="w-6 h-6 m-0.5 border border-white bg-white bg-opacity-20 backdrop-blur-md"></div>
+    )
+  }
   return (
     <Image
       src={`http://localhost:8000/static/dragontail-14.15.1/14.15.1/img/item/${itemID}.png`}
