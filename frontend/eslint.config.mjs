2b0a6e09657618d28612a55528deebff80b831aa
DominikSukar <dominik.sukar@gmail.com>
2024-08-13 21:49:16 +0200

    chore: upgrade typescript package

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,11 +1,22 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import * as tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 
 export default [
-  {files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
+    languageOptions: { 
+      globals: globals.browser,
+      parser: tsParser,
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
