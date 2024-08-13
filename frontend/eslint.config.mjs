48f54f4a56b46ccc121e7870b075730c94dec589
DominikSukar <dominik.sukar@gmail.com>
2024-08-13 21:51:16 +0200

    Revert "chore: upgrade typescript package"
    This reverts commit 2b0a6e09657618d28612a55528deebff80b831aa.

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,22 +1,11 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import * as tseslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
+import tseslint from "typescript-eslint";
+
 
 export default [
-  {
-    files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
-    languageOptions: { 
-      globals: globals.browser,
-      parser: tsParser,
-      parserOptions: {
-        project: "./tsconfig.json",
-      },
-    },
-    plugins: {
-      "@typescript-eslint": tseslint,
-    },
-  },
+  {files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"]},
+  {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
