5f59e19425aa63c0ba295e1fab7a1c581cabc1da
DominikSukar <dominik.sukar@gmail.com>
2024-08-13 21:44:20 +0200

    chore: modify frontend linting worklow for eslint to include jsx and tsx files

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -4,7 +4,7 @@ import tseslint from "typescript-eslint";
 
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"]},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
