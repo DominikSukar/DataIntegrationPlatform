31b1dec686af333644941d95a9f240e77402e5cc
DominikSukar <dominik.sukar@gmail.com>
2024-08-18 20:04:36 +0200

    feat: add tooltip for shadcn and apply it to champion icon

--- frontend/components/game/ChampionIcon.tsx
+++ frontend/components/game/ChampionIcon.tsx
@@ -1,4 +1,10 @@
 import Image from "next/image";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
 
 function ChampionIcon({
   championName,
@@ -8,13 +14,22 @@ function ChampionIcon({
   size: number;
 }) {
   return (
-    <Image
-      src={`http://localhost:8000/static/dragontail-14.15.1/14.15.1/img/champion/${championName}.png`}
-      className="m-0.5"
-      width={size}
-      height={size}
-      alt={`Champion ${championName}`}
-    />
+    <TooltipProvider>
+      <Tooltip delayDuration={200}>
+        <TooltipTrigger>
+          <Image
+            src={`http://localhost:8000/static/dragontail-14.15.1/14.15.1/img/champion/${championName}.png`}
+            className="m-0.5"
+            width={size}
+            height={size}
+            alt={`Champion ${championName}`}
+          />
+        </TooltipTrigger>
+        <TooltipContent className="bg-slate-700 bg-opacity-90 backdrop-blur-md rounded-full border-2">
+          <p>{championName}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 }
 
