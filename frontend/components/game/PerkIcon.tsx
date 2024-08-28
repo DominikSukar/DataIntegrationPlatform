8bedb00bcff46d45ee8d93b61943fe0b9ad6b11f
DominikSukar <dominik.sukar@gmail.com>
2024-08-29 00:36:35 +0200

    feat: add perk component

---
+++ frontend/components/game/PerkIcon.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
+import ItemDescription from "./ItemDescription";
+
+import { fetchSummoners } from "@/api/datadragon";
+
+const PerkIcon = async({ perkID, size }: { perkID: number; size: number }) => {
+  const summoners = await fetchSummoners();
+  const summoner = summoners[perkID]
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={200} disableHoverableContent={true}>
+        <TooltipTrigger>
+          <Image
+            src={`http://localhost:8000/static/dragontail-14.15.1/perk-images-byID/perk-images/Styles/${perkID}.png`}
+            className="m-0.5"
+            width={size}
+            height={size}
+            alt={`Item ${perkID}`}
+          />
+        </TooltipTrigger>
+      </Tooltip>
+    </TooltipProvider>    
+  );
+}
+
+export default PerkIcon;
