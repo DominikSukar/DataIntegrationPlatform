02af8adb2c401107beef779ade6dbcdf198817c2
DominikSukar <dominik.sukar@gmail.com>
2024-09-08 23:10:35 +0200

    feat: add support for case when user is not found

--- frontend/app/(root)/summoner/[server]/[summonername]/page.tsx
+++ frontend/app/(root)/summoner/[server]/[summonername]/page.tsx
@@ -1,6 +1,9 @@
 import User from "./User";
 import Game from "./Game";
 
+import Logo from "@/components/Logo";
+import ProfileForm from "@/components/forms/Form";
+
 import { MatchData, PageProps } from "@/types/matchTypes";
 import { DOMAIN } from "@/constants/api";
 
@@ -11,6 +14,27 @@ const SummonerPage = async ({ params }: PageProps) => {
     `${DOMAIN}/match_history/${server}/?summoner_name=${summonerName}`,
     { next: { revalidate: 60 } }
   );
+  if (!response.ok) {
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen">
+          <div className="p-5 bg-white bg-opacity-20 backdrop-blur-md border-2 border-white border-opacity-30 rounded-[10px] overflow-hidden text-center">
+            <h1 className="flex justify-center">
+              <p>User</p>
+              <p className="ml-1 text-red-600">{summonerName}</p>
+              <p className="ml-1">not found</p>
+              </h1>
+            <h3 className="flex justify-center">
+              <p className="text-red-600">{response.status}</p>
+              <p className="ml-1">{response.statusText}</p>
+            </h3>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <Logo></Logo>
+            <ProfileForm></ProfileForm>
+          </div>
+      </div>
+    );
+  }
   const matches: MatchData[] = await response.json();
 
   return (
