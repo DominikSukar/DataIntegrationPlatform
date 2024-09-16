c36eeb8b393de18c144f54c219690abd0522d4a8
DominikSukar <dominik.sukar@gmail.com>
2024-09-16 23:21:42 +0200

    feat: add necessary imports in database/__init__ so sqlalchemy lazy imports the relationships

---
+++ backend/database/__init__.py
@@ -0,0 +1,5 @@
+from database.database import Base
+from database.models.summoner.summoner import Summoner
+from database.models.other.server import Server
+
+__all__ = ["Base", "Summoner", "Server"]
