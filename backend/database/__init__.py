ba373c1d52dea0550c8763465cd7c1142362f315
DominikSukar <dominik.sukar@gmail.com>
2024-09-20 00:06:03 +0200

    feat: add tables from previous commit to main init file

--- backend/database/__init__.py
+++ backend/database/__init__.py
@@ -4,6 +4,8 @@ from database.models.summoner.server import Server
 from database.models.summoner.summoner_duos import SummonerDuo
 from database.models.match.match import Match
 from database.models.match.match_team import MatchTeam
+from database.models.match.match_participant import MatchParticipant
+from backend.database.models.match.match_team_summoner import MatchTeamSummoner
+from backend.database.models.summoner.summoner_champion_stats import SummonerChampionStat
 
-
-__all__ = ["Base",  "Server", "Summoner", "SummonerDuo", "Match", "MatchTeam"]
+__all__ = ["Base",  "Server", "Summoner", "SummonerDuo", "Match", "MatchTeam", "MatchParticipant", "MatchTeamSummoner", "SummonerChampionStat"]
