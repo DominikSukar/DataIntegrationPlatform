d55ee235c1911990306cc8f0ba5374cae8894111
DominikSukar <dominik.sukar@gmail.com>
2024-09-20 23:22:41 +0200

    feat: adjust init file with new imports for alembic

--- backend/database/__init__.py
+++ backend/database/__init__.py
@@ -1,11 +1,36 @@
 from database.database import Base
-from database.models.summoner.summoner import Summoner
-from database.models.summoner.server import Server
-from database.models.summoner.summoner_duos import SummonerDuo
-from database.models.match.match import Match
-from database.models.match.match_team import MatchTeam
-from database.models.match.match_participant.match_participant import MatchParticipant
-from database.models.match.match_team_summoner import MatchTeamSummoner
-from database.models.summoner.summoner_champion_stats import SummonerChampionStat
 
-__all__ = ["Base",  "Server", "Summoner", "SummonerDuo", "Match", "MatchTeam", "MatchParticipant", "MatchTeamSummoner", "SummonerChampionStat"]
+from database.models.basic import Champion, Item, Perk, SummonerSpell, Server
+from database.models.summoner import SummonerChampionStat, SummonerDuo, Summoner
+from database.models.match import (
+    Match,
+    MatchTeam,
+    MatchTeamSummoner,
+    MatchTeamChampionBans,
+)
+from database.models.match.match_participant import (
+    MatchParticipant,
+    MatchParticipantSummonerItem,
+    MatchParticipantPerk,
+    MatchParticipantSummonerSpell,
+)
+
+__all__ = [
+    "Base",
+    "Champion",
+    "Item",
+    "Perk",
+    "SummonerSpell",
+    "Server",
+    "Summoner",
+    "SummonerDuo",
+    "Match",
+    "MatchTeam",
+    "MatchParticipant",
+    "MatchTeamSummoner",
+    "SummonerChampionStat",
+    "MatchTeamChampionBans",
+    "MatchParticipantSummonerItem",
+    "MatchParticipantPerk",
+    "MatchParticipantSummonerSpell",
+]
