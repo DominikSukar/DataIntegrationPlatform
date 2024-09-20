808c427cad2fa7d122dd8d36729435a2d823523a
DominikSukar <dominik.sukar@gmail.com>
2024-09-20 23:24:44 +0200

    refactor: change file names containing table models to fit class names

--- backend/database/models/summoner/__init__.py
+++ backend/database/models/summoner/__init__.py
@@ -1,5 +1,5 @@
-from .summoner_champion_stats import SummonerChampionStat
-from .summoner_duos import SummonerDuo
+from .summoner_champion_stat import SummonerChampionStat
+from .summoner_duo import SummonerDuo
 from .summoner import Summoner
 
 __all__ = ["SummonerChampionStat", "SummonerDuo", "Summoner"]
