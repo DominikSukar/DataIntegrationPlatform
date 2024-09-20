3ecf37cd36c58872bea2c7045435532bab28af68
DominikSukar <dominik.sukar@gmail.com>
2024-09-20 22:36:58 +0200

    refactor: remove unsued file

--- backend/database/__init__.py
+++ backend/database/__init__.py
@@ -4,8 +4,8 @@ from database.models.summoner.server import Server
 from database.models.summoner.summoner_duos import SummonerDuo
 from database.models.match.match import Match
 from database.models.match.match_team import MatchTeam
-from database.models.match.match_participant import MatchParticipant
-from backend.database.models.match.match_team_summoner import MatchTeamSummoner
-from backend.database.models.summoner.summoner_champion_stats import SummonerChampionStat
+from database.models.match.match_participant.match_participant import MatchParticipant
+from database.models.match.match_team_summoner import MatchTeamSummoner
+from database.models.summoner.summoner_champion_stats import SummonerChampionStat
 
 __all__ = ["Base",  "Server", "Summoner", "SummonerDuo", "Match", "MatchTeam", "MatchParticipant", "MatchTeamSummoner", "SummonerChampionStat"]
