1bc6865d7a269a41d7eec14edce176dc79f49c07
DominikSukar <dominik.sukar@gmail.com>
2024-09-20 23:19:48 +0200

    feat: add summoner and summoner_duo tables. delete unused server file

---
+++ backend/database/models/summoner/__init__.py
@@ -0,0 +1,5 @@
+from .summoner_champion_stats import SummonerChampionStat
+from .summoner_duos import SummonerDuo
+from .summoner import Summoner
+
+__all__ = ["SummonerChampionStat", "SummonerDuo", "Summoner"]
--- backend/database/models/summoner/summoner.py
+++ backend/database/models/summoner/summoner.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
 
 from database.database import Base
 
