4c8394328382f7de81a4f7c01e4781711de0fddb
DominikSukar <dominik.sukar@gmail.com>
2024-09-21 23:59:58 +0200

    feat: create a database router tag for future use

---
+++ backend/routers/database.py
@@ -0,0 +1,36 @@
+import logging
+from typing import Any, Annotated
+
+from fastapi import APIRouter, Query, Path
+from models import SummonerAndSpectorServerModel
+from api_requests.spectator import SpectatorController
+from schemas import CurrentGameInfo
+from utils.wrappers import map_puuid_and_server
+
+logger = logging.getLogger(__name__)
+router = APIRouter()
+
+
+@router.get("/{server}/match/{match_ID}")
+@map_puuid_and_server
+async def get_match(
+    server: SummonerAndSpectorServerModel,
+    mapped_server: Any = Query(None, include_in_schema=False),
+    summoner_name: str = None,
+    puuid: str = None,
+) -> CurrentGameInfo:
+    """Gets data about a specific match from a database. 
+    Takes into account the user that requested it."""
+    pass
+
+    return "Not working"
+
+@router.post("/{server}/match/{match_ID}")
+async def post_match(
+    server: SummonerAndSpectorServerModel,
+    match_ID: Annotated[str, Path(example="EUW1_7091585440")],
+) -> CurrentGameInfo:
+    "Posts data about a specific match from a database"
+    pass
+
+    return "Not working"
