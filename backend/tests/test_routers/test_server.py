cff4903953be825978ca761a927969f654cafdf5
DominikSukar <dominik.sukar@gmail.com>
2024-09-24 22:25:17 +0200

    test: update tests to check for number of keys returned in response and whether id is equal to 1

--- backend/tests/test_routers/test_server.py
+++ backend/tests/test_routers/test_server.py
@@ -48,12 +48,16 @@ class TestServerAPI:
         response = mock_client.get("/server/")
         assert response.status_code == 200
 
-        assert len(response.json()) == 1
-
         first_object = response.json()[0]
+        # We check for 6 keys since 'id' should be returned as well (so 5+1 keys)
+        assert len(first_object) == 6
+
         assert "id" in first_object
-        del first_object["id"]
-        assert first_object == sample_server_data
+        assert first_object["full_name"] == sample_server_data["full_name"]
+        assert first_object["symbol"] == sample_server_data["symbol"]
+        assert first_object["riot_symbol"] == sample_server_data["riot_symbol"]
+        assert first_object["hostname"] == sample_server_data["hostname"]
+        assert first_object["active"] == sample_server_data["active"]
 
     def test_get(self, mock_client, sample_server_data, created_server):
         """
@@ -62,11 +66,16 @@ class TestServerAPI:
         response = mock_client.get(f"/server/{created_server["id"]}")
         assert response.status_code == 200
 
-        assert created_server["full_name"] == sample_server_data["full_name"]
-        assert created_server["symbol"] == sample_server_data["symbol"]
-        assert created_server["riot_symbol"] == sample_server_data["riot_symbol"]
-        assert created_server["hostname"] == sample_server_data["hostname"]
-        assert created_server["active"] == sample_server_data["active"]
+        returned_object = response.json()
+        # We check for 6 keys since 'id' should be returned as well (so 5+1 keys)
+        assert len(returned_object) == 6
+        
+        assert "id" in returned_object
+        assert returned_object["full_name"] == sample_server_data["full_name"]
+        assert returned_object["symbol"] == sample_server_data["symbol"]
+        assert returned_object["riot_symbol"] == sample_server_data["riot_symbol"]
+        assert returned_object["hostname"] == sample_server_data["hostname"]
+        assert returned_object["active"] == sample_server_data["active"]
 
     def test_patch(self, mock_client, created_server, sample_server_data):
         """
@@ -76,12 +85,15 @@ class TestServerAPI:
         response = mock_client.patch(f"/server/{created_server['id']}", json=update_data)
         assert response.status_code == 200
 
-        assert response.json()["active"] == False # noqa: E712
+        returned_object = response.json()
+        # We check for 6 keys since 'id' should be returned as well (so 5+1 keys)
+        assert len(returned_object) == 6
 
-        assert created_server["full_name"] == sample_server_data["full_name"]
-        assert created_server["symbol"] == sample_server_data["symbol"]
-        assert created_server["riot_symbol"] == sample_server_data["riot_symbol"]
-        assert created_server["hostname"] == sample_server_data["hostname"]
+        assert returned_object["active"] == False # noqa: E712
+        assert returned_object["full_name"] == sample_server_data["full_name"]
+        assert returned_object["symbol"] == sample_server_data["symbol"]
+        assert returned_object["riot_symbol"] == sample_server_data["riot_symbol"]
+        assert returned_object["hostname"] == sample_server_data["hostname"]
 
     def test_delete(self, mock_client, created_server):
         """
@@ -91,5 +103,6 @@ class TestServerAPI:
         delete_response = mock_client.delete(f"/server/{created_server['id']}")
         assert delete_response.status_code == 200
 
-        get_response = mock_client.get(f"/server/{created_server['id']}")
+        # We check if object with 'id' returned from previous DELETE does not exist anymore
+        get_response = mock_client.get(f"/server/{delete_response.json()["id"]}")
         assert get_response.status_code == 404        
