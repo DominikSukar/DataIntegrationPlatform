78cf21b5e2edca50d8436167e3339db3d3f239b1
DominikSukar <dominik.sukar@gmail.com>
2024-09-29 12:00:38 +0200

    feat: add data about rank, lp, matched_played, matches_won and winrate to 'summoners' table

--- backend/database/models/summoner/summoner.py
+++ backend/database/models/summoner/summoner.py
@@ -1,4 +1,4 @@
-from sqlalchemy import ForeignKey, Integer, String
+from sqlalchemy import ForeignKey, Integer, SmallInteger, Float, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from database.database import Base
@@ -15,6 +15,11 @@ class Summoner(Base):
     nickname: Mapped[str] = mapped_column(String(20), nullable=False)
     tag: Mapped[str] = mapped_column(String(5), nullable=False)
     server_id: Mapped[int] = mapped_column(Integer, ForeignKey("servers.id"))
+    rank: Mapped[str] = mapped_column(String(20), nullable=False)
+    lp = Mapped[int] = mapped_column(SmallInteger, nullable=False)
+    matches_played = Mapped[int] = mapped_column(SmallInteger, nullable=False)
+    matched_won = Mapped[int] = mapped_column(SmallInteger, nullable=False)
+    winrate = Mapped[int] = mapped_column(Float, nullable=False)
 
     def __repr__(self):
         return f"<Summoner(nickname='{self.nickname}', tag='{self.tag}', server_id='{self.server_id}')>"
