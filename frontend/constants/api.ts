bcfdd6748df2cfbd466bb20e7e4bd4e0fd11a11d
DominikSukar <dominik.sukar@gmail.com>
2024-09-29 16:20:50 +0200

    feat: add new constants for frontend service which are URLs pointing to backend's served static files

--- frontend/constants/api.ts
+++ frontend/constants/api.ts
@@ -1,1 +1,4 @@
 export const DOMAIN = "http://localhost:8000"
+export const ITEM_URL = `${DOMAIN}/static/items`
+export const PERK_URL = `${DOMAIN}/static/perks`
+export const SUMMONER_SPELL_URL = `${DOMAIN}/static/summoner_spells`
