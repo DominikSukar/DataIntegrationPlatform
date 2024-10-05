b8802830ab1a11388e202b1a3549ba6c1376641c
DominikSukar <dominik.sukar@gmail.com>
2024-10-05 21:09:25 +0200

    feat: add date field and 'seasons' foreign key to 'matches'

--- backend/database/models/match/match.py
+++ backend/database/models/match/match.py
@@ -1,5 +1,7 @@
-from sqlalchemy import ForeignKey, String
+from sqlalchemy import ForeignKey, String, DateTime
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.sql import func
+from datetime import datetime
 
 from database.database import Base
 
@@ -15,6 +17,10 @@ class Match(Base):
     riot_match_id: Mapped[str] = mapped_column(String(50), nullable=False)
     server_id: Mapped[int] = mapped_column(ForeignKey("servers.id"), nullable=False)
     game_result: Mapped[str] = mapped_column(String, nullable=False)
+    date: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+    season_id: Mapped[int] = mapped_column(ForeignKey("seasons.id"), nullable=False)
 
     def __repr__(self):
-        return f"<Match(id='{self.id}', riot_match_id='{self.riot_match_id}')>"
+        return f"<Match(id='{self.id}', riot_match_id='{self.riot_match_id}'), date='{self.date}'>"
