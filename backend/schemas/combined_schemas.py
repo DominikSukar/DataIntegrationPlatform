25b50b0b3eb5de71b38b8de5329e27d6fe43bef9
DominikSukar <dominik.sukar@gmail.com>
2024-10-07 23:10:44 +0200

    refactor: remove none types from 'LeagueAndSummonerEntryDTO' schema

--- backend/schemas/combined_schemas.py
+++ backend/schemas/combined_schemas.py
@@ -10,14 +10,14 @@ class LeagueAndSummonerEntryDTO(BaseModel):
     summonerLevel: float
     leagueId: str | None = None
     summonerId: str | None = None
-    queueType: str | None = None
-    tier: str | None = None
-    rank: str | None = None
-    leaguePoints: int | None = None
-    wins: int | None = None
-    losses: int | None = None
-    winrate: int | None = None
-    hotStreak: bool | None = None
-    veteran: bool | None = None
-    freshBlood: bool | None = None
-    inactive: bool | None = None
+    queueType: str
+    tier: str
+    rank: str
+    leaguePoints: int
+    wins: int
+    losses: int
+    winrate: int
+    hotStreak: bool
+    veteran: bool
+    freshBlood: bool
+    inactive: bool
