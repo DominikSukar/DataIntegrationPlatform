d46f544eefd632ffff06ea2e4e06100398d06415
DominikSukar <dominik.sukar@gmail.com>
2024-11-20 23:31:44 +0100

    fix: pydantic's model of 'perk' has description attribute type hinted as optional

--- backend/serializers/basic/perk.py
+++ backend/serializers/basic/perk.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel, Field, ConfigDict
 class PerkBase(BaseModel):
     riot_id: int = Field(..., examples=[8112])
     name: str = Field(..., max_length=20, examples=["Electrocute"])
-    description: str = Field(
+    description: Optional[str] = Field(
         ...,
         examples=[
             "Hitting a champion with 3 <b>separate</b> attacks or abilities within 3s deals bonus <lol-uikit-tooltipped-keyword key='LinkTooltip_Description_AdaptiveDmg'><font color='#48C4B7'>adaptive damage</font></lol-uikit-tooltipped-keyword>.<br><br>Damage: 50 - 190 (+0.1 bonus AD, +0.05 AP) damage.<br><br>Cooldown: 20s<br><br><hr><i>'We called them the Thunderlords, for to speak of their lightning was to invite disaster.'</i>"
